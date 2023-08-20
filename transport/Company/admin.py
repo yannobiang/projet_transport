12dde5a24c0c9bd353ad99bdddd98fe427c1e591
yannobaing <princeobaing@gmail.com>
2023-08-20 15:08:25 +0200

    la version complete model compagnie

--- transport/Company/admin.py
+++ transport/Company/admin.py
@@ -1,7 +1,49 @@
 from django.contrib import admin
-from Company.models import company
+from Company.models import (Transporteurs, Voyageurs, 
+                            Asso_trans_voyageur, Voyages,
+                            Compagnie, Transports)
 # Register your models here.
-class AdminCompany(admin.ModelAdmin) :
-    list_display = ()
+class AdminTransporteurs(admin.ModelAdmin) :
+    list_display = ("nom", 
+    "prenom",
+    "date_de_naissance",
+    "adresse",
+    "ville",
+    "permis",
+    "phone",
+    "email" )
 
-admin.site.register(Company, AdminCompany)
+class AdminVoyageurs(admin.ModelAdmin) :
+    list_display = ("nom_V",
+    "prenom_V",
+    "email")
+
+class AdminAsso_trans_voyageur(admin.ModelAdmin) :
+    list_display = ("voyageurs",  "transporteurs")
+
+class AdminVoyages(admin.ModelAdmin) :
+    list_display = ("date_depart",
+    "date_arrivee", 
+    "ville_depart",
+    "ville_arrivee",
+    "prix_unitaire",
+    "transporteurs")
+
+class AdminCompagnie(admin.ModelAdmin) :
+    list_display = ("nom_E",
+    "siren",
+    "transporteurs")
+class AdminTransports(admin.ModelAdmin) :
+    list_display = ("marque",
+    "matricule",
+    "nombre_de_place",
+    "voyages",
+    "compagnie")
+
+
+admin.site.register(Transporteurs, AdminTransporteurs)
+admin.site.register(Voyageurs, AdminVoyageurs)
+admin.site.register(Asso_trans_voyageur, AdminAsso_trans_voyageur)
+admin.site.register(Voyages, AdminVoyages)
+admin.site.register(Compagnie, AdminCompagnie)
+admin.site.register(Transports, AdminTransports)
--- transport/Company/models.py
+++ transport/Company/models.py
@@ -2,5 +2,77 @@ from django.db import models
 
 # Create your models here.
 
-class company(models.Model) :
+class Transporteurs(models.Model) :
+
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    date_de_naissance = models.DateField()
+    adresse = models.TextField()
+    ville = models.CharField(max_length=30)
+    permis = models.CharField(max_length=5, default = "")
+    phone = models.CharField(max_length= 60)
+    email = models.EmailField()
+
+    class Meta :
+        verbose_name = ("Transporteur")
+        verbose_name_plural = ("Transporteurs")
+        ordering = ["nom","prenom"]
+    def __str__(self) -> str:
+        return self.name
+
+class Voyageurs(models.Model) :
+
+    nom_V = models.CharField(max_length=50)
+    prenom_V = models.CharField(max_length=50)
+    email = models.EmailField()
+    class Meta :
+        verbose_name = ("Voyageur")
+        verbose_name_plural = ("Voyageurs")
+    def __str__(self) -> str:
+        return self.name
+
+class Asso_trans_voyageur(models.Model):
+    voyageurs = models.ForeignKey(Voyageurs, on_delete=models.CASCADE)
+    transporteurs = models.ForeignKey(Transporteurs, on_delete=models.CASCADE)
+
+
+class Voyages(models.Model) :
     
+    date_depart = models.DateTimeField()
+    date_arrivee = models.DateTimeField()
+    ville_depart = models.CharField(max_length=50)
+    ville_arrivee = models.CharField(max_length=50)
+    prix_unitaire = models.FloatField()
+    transporteurs = models.ForeignKey(Transporteurs, on_delete=models.CASCADE)
+    class Meta :
+        verbose_name = ("Voyage")
+        verbose_name_plural = ("Voyages")
+        
+    def __str__(self) -> str:
+        return self.name
+class Compagnie(models.Model):
+
+    nom_E = models.CharField(max_length=50)
+    siren = models.BigIntegerField()
+    transporteurs = models.ForeignKey(Transporteurs, on_delete=models.CASCADE)
+
+    class Meta :
+        verbose_name = ("Compagnie")
+        verbose_name_plural = ("Compagnies")
+        
+    def __str__(self) -> str:
+        return self.name
+
+class Transports(models.Model):
+    marque = models.CharField(max_length=50)
+    matricule = models.CharField(max_length=50, default="")
+    nombre_de_place = models.IntegerField()
+    voyages = models.ForeignKey(Voyages, on_delete=models.CASCADE)
+    compagnie = models.ForeignKey(Compagnie, on_delete=models.CASCADE)
+
+    class Meta :
+        verbose_name = ("Transport")
+        verbose_name_plural = ("Transports")
+        
+    def __str__(self) -> str :
+        return self.name
