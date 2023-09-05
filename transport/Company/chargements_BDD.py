a50f04a6e507f54949b5513a72ffe0e00509b26e
yannobaing <princeobaing@gmail.com>
2023-09-05 22:21:25 +0200

    add data from shell django

--- transport/Company/chargements_BDD.py
+++ transport/Company/chargements_BDD.py
@@ -1,48 +1,83 @@
-from Company.models import (Transporteurs, Voyageurs, 
-                     Voyages, Compagnie, Transports
-                     )
-from datetime import (tzinfo, timedelta, datetime, timezone)
+from Company.models import (Transporteurs, Voyageurs, Voyages, Compagnie, Transports)
 
+class fillData():
 
-def addDateTimes(AAAA, MM, DD, HH, MN, SS=00):
-    """ cette fonction permet de remplir le champs datetime dans la bdd """
-    return datetime(year = AAAA, month = MM, day = DD, hour = HH, minute = MN, second = SS)
+    """ Cette classe permet de surcharger les données dans la BD
+        Il n ' y a que Transporteurs que j'ai reussi à charger
+        Le problème se pose au niveau des cles primaire contrainte.
+        Les deux tables transporteurs et voyageurs ont ete chargées
+    """
 
-dataTransporteurs = {
-    "name" : ["Charly", "Chapart", "Martin", "Martial"],
-    "firstname" :["Jean", "Pratick", "Chaperon", "Jean-Chalonçon"] ,
-    "date_de_naissance" :["1994-02-28", "1989-10-30", "1988-05-05", "1994-03-14"] ,
-    "ville" :["ST Dénis", "Oyem", "Port-Gentil", "Paris"] ,
-    "permis" :["C", "B", "B", "D"] ,
-    "phone" : ["02854732", "06854512", "01528496", "04528694"],
-    "email" : ["charlyjean@popo.fr", "chapartpratick@popo.ga", "chaperonmartin@papa.fr", "martial.jean-chaloncon@aol.com"]
-}
+    def __init__(self, nom, data, model):
 
-dataVoyageurs = {
-    "name" : ["OBIANG", "MAPANGOU", "MOUNGUEGUI", "OYANE", "ANGUE"],
-    "firstname" :["Yann", "Francis", "Candy", "Jose", "Marie-Claire"],
-    "email" :["yannobiang@jiji.com", "francismapangou@popo.com", "candymounguegui@yahoo.fr", "joseoyane@gpail.com", "marieclaireangue@yahoo.fr"]
-}
+        self.nom = nom
+        self.data = data 
+        self.model = model 
+        
 
-dataVoyages = {
-    "date_depart" : [addDateTimes(2022, 8, 5, 14, 00), addDateTimes(2023, 9, 22, 12, 40), addDateTimes(2022, 5, 22, 16, 30),
-    addDateTimes(2023, 3, 16, 2, 50), addDateTimes(2022, 6, 16, 10, 28), addDateTimes(2023, 1, 31, 23, 35)],
-    "date_arrivee" : [addDateTimes(2022, 8, 5, 10, 00), addDateTimes(2023, 9, 12, 12, 40), addDateTimes(2022, 5, 30, 16, 30),
-    addDateTimes(2023, 3, 15, 21, 15), addDateTimes(2022, 6, 15, 20, 38), addDateTimes(2023, 1, 31, 20, 25)],
-    "ville_depart" : ["OYEM", "LIBREVILLE", "PORT-GENTIL", "FRANCEVILLE", "NTOUM"],
-    "ville_arrivee" : ["LIBREVILLE", "PORT-GENTIL", "LIBREVILLE", "OYEM", "LIBREVILLE"],
-    "prix_unitaire" :[7000, 20000, 20000, 50000, 1000]
+    def charge(self):
+        if self.nom == "dataTransporteurs":
 
-}
+            for element in range(len(self.data["name"])):
+                New = self.model.objects.create(
+                    name = self.data["name"][element],
+                    firstname = self.data["firstname"][element],
+                    date_de_naissance = self.data["date_de_naissance"][element],
+                    ville = self.data["ville"][element],
+                    permis = self.data["permis"][element],
+                    phone = self.data["phone"][element],
+                    email = self.data["email"][element]
+                )
+                New.save()
+        elif self.nom == "dataVoyageurs":
+            for element in range(len(self.data["name"])):
+                New = self.model.objects.create(
+                    name = self.data["name"][element],
+                    firstname = self.data["firstname"][element],
+                    email = self.data["email"][element]
+                )
+                New.save()
 
-dataTransports = {
-    "marque" : ["4x4 Pajero", "Bus 4x4 Climatise", "Toyota Yaris", "Toyota Corolla", "Carina"],
-    "matricule" : ["GALIB-125486", "GAOYE-258963", "GAFRA-147852", "GALIB-951753", "GALIB-357159", "GALIB-987123"], 
-    "nombre_de_place" :[50, 30, 20, 4, 5, 50]
-    
-}
+        elif self.nom == "dataVoyages":
+            porteurs = Transporteurs.objects.all()
 
-dataCompany = {
-    "name" : ["Major", "Setrag", "Ndong Sima", "Major", "Ndong Sima", "Major", "GTM"],
-    "siren" : [123456, 456789, 654321, 987321, 951741, 258456, 985325]
-}
+            for element in range(len(self.data["ville_depart"])):
+                New = self.model.objects.create(
+                    date_depart = self.data["date_depart"][element],
+                    date_arrivee = self.data["date_arrivee"][element],
+                    ville_depart = self.data["ville_depart"][element],
+                    ville_arrivee = self.data["ville_arrivee"][element],
+                    prix_unitaire = self.data["prix_unitaire"][element],
+                    transporteurs = porteurs[element]
+                )
+                New.save()
+        elif self.nom == "dataTransports":
+            voyagesFirst = Voyages.objects.all()
+            compagnieFirst = Compagnie.objects.all()
+
+            for element in range(len(self.data["marque"])):
+                New = self.model.objects.create(
+                    marque = self.data["marque"][element],
+                    matricule = self.data["matricule"][element],
+                    nombre_de_place = self.data["nombre_de_place"][element],
+                    voyages = voyagesFirst[element+1],
+                    compagnie = compagnieFirst[element+1]
+                )
+                New.save()
+
+        elif self.nom == "dataCompagnie":
+            porteurs = Transporteurs.objects.all()
+            for element in range(len(self.data["name"])):
+                New = self.model.objects.create(
+                    name = self.data["name"][element],
+                    siren = self.data["siren"][element],
+                    transporteurs = porteurs[element+1]
+                    
+                )
+                New.save()
+        elif self.nom == "Asso_trans_voyageur":
+            New = self.model.objects.create(
+            voyageurs = Voyageurs.objects.all(),
+            transporteurs = Transporteurs.objects.all()
+            )
+            New.save()
