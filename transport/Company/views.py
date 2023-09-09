d043dc6e3e7d10843c89ba406c6d4d48ab6dc885
yannobaing <princeobaing@gmail.com>
2023-09-09 15:06:49 +0200

    add data from shell django

--- transport/Company/views.py
+++ transport/Company/views.py
@@ -1,3 +1,77 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+
+    """cette fonction lance la page home du site"""
+
+    return render(request, 'html/index.html')
+
+def about(request):
+    """
+        Cette fonction permet de parcourir 
+        la page au sujet de nous
+    """
+    return render(request, 'html/about.html')
+
+def homepage2(request):
+
+    """
+        cette fonction permet de s'orienter vers
+        la deuxieme page du site internet 
+    """
+    return render(request, 'html/homepage-2.html')
+
+def homepage3(request):
+
+    """
+        cette fonction permet de s'orienter vers
+        la troisieme page du site internet
+    """
+    return render(request, 'html/homepage-3.html')
+
+def indisponible(request):
+
+    """
+        cette fonction permet de erreur 404
+    """
+    return render(request, 'html/404.html')
+
+def question(request):
+
+    """
+        la page de la foire aux questions
+        qui réponds à la plus part des questions que
+        se posent les users
+    """
+
+    return render(request, 'html/FAQ.html')
+
+def contact(request):
+
+    return render(request, 'html/contact.html')
+
+
+def comming_soon(request):
+    
+    """
+        la fonction permet d'afficher 
+        comming soon de la page
+    """
+    return render(request, 'html/comming-soon.html')
+
+def career(request):
+    
+    """
+        la fonction qui oriente vers la page carriere du site
+    """
+    return render(request, 'html/career.html')
+
+def sign_in(request):
+     """ la fonction qui permet de s'identifier """
+
+     return render(request, 'html/sign-in.html')
+
+
