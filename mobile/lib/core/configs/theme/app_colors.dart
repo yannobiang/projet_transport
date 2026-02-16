e18cd73bc983f62590a7985e1c820864dd22c0dc
Brasco19 <ebangabdon641@gmail.com>
2026-02-16 21:18:39 +0900

    orhanization of mobile folder

---
+++ mobile/lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const primary = Color(0xff1e88e5);
+  static const lightBackground = Color(0xfff2f2f2);
+  static const darkBackground = Color(0xff0d0c0c);
+  static const grey = Color(0xffbebebe);
+  static const darkGrey = Color(0xff343434);
+}
---
+++ mobile/lib/presentation/splash/pages/splash.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mobile/core/configs/assets/app_vectors.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Center(
+      child: SvgPicture.asset(
+        AppVectors.logo
+        ),
+    ),
+    );
+  }
+}
